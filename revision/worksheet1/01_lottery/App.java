e7bd5c1b08c8ea2627999c7f9ca5ff1d0aab9214
Harrison Black <harrison.black18@Gmail.com>
2024-07-19 09:30:55 +1000

    refactor

--- revision/worksheet1/01_lottery/App.java
+++ revision/worksheet1/01_lottery/App.java
@@ -1,32 +1,40 @@
 import java.util.Scanner;
 
 public class App {
-	public static void main(String[] args) {
-		// Initialise and Scanner
-		Scanner scan = new Scanner(System.in);
 
-		// Get random 2 digit integer
-		int r = (int) Math.floor(Math.random() * 89) + 10;
-		int rF = Integer.parseInt(Integer.toString(r).substring(0, 1));
-		int rS = Integer.parseInt(Integer.toString(r).substring(1, 2));
+    public static void main(String[] args) {
+        // Initialise and Scanner
+        Scanner scan = new Scanner(System.in);
 
-		// Get user guess
-		System.out.print("Guess a number from 1-99\n> ");
-		int g = scan.nextInt();
+        // Get random 2 digit integer
+        int r = (int) Math.floor(Math.random() * 89) + 10;
+        int rF = Integer.parseInt(Integer.toString(r).substring(0, 1));
+        int rS = Integer.parseInt(Integer.toString(r).substring(1, 2));
 
-		int gF = Integer.parseInt(Integer.toString(g).substring(0, 1));
-		int gS = Integer.parseInt(Integer.toString(g).substring(1, 2));
+        // Get user guess
+        System.out.print("Guess a number from 1-99\n> ");
+        int g = scan.nextInt();
 
-		// Compare input and random
-		if (Integer.compare(r, g) == 0) {
-			System.out.println("Exact match: you win $10000");
-		} else if ((Integer.compare(rF, gS) == 0) && (Integer.compare(rS, gF) == 0)) {
-			System.out.println("Match all digits: you won $3000");
-		} else if ((Integer.compare(rF, gS) == 0) || (Integer.compare(rS, gF) == 0) || (Integer.compare(rS, gS) == 0)
-				|| (Integer.compare(rF, gF) == 0)) {
-			System.out.println("Match one digit: you won $1000");
-		} else {
-			System.out.println("Sorry, no match");
-		}
-	}
+        int gF = Integer.parseInt(Integer.toString(g).substring(0, 1));
+        int gS = Integer.parseInt(Integer.toString(g).substring(1, 2));
+
+        // Compare input and random
+        if (Integer.compare(r, g) == 0) {
+            System.out.println("Exact match: you win $10000");
+        } else if (
+            (Integer.compare(rF, gS) == 0) && (Integer.compare(rS, gF) == 0)
+        ) {
+            System.out.println("Match all digits: you won $3000");
+        } else if (
+            (Integer.compare(rF, gS) == 0) ||
+            (Integer.compare(rS, gF) == 0) ||
+            (Integer.compare(rS, gS) == 0) ||
+            (Integer.compare(rF, gF) == 0)
+        ) {
+            System.out.println("Match one digit: you won $1000");
+        } else {
+            System.out.println("Sorry, no match");
+        }
+        scan.close();
+    }
 }
--- revision/worksheet2/01_digitSum/App.java
+++ revision/worksheet2/01_digitSum/App.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
 public class App {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 
-		String str;
-		char[] digits;
-		int in;
-		int out = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		System.out.print("Enter an Integer\n>");
-		in = Integer.parseInt(sc.nextLine());
-		str = Integer.toString(in);
-		for (int i = 0; i < str.length(); i++) {
-			out += Character.getNumericValue(str.charAt(i));
-		}
-		System.out.println("Total = " + out);
-	}
+        String str;
+        int in;
+        int out = 0;
+
+        System.out.print("Enter an Integer\n>");
+        in = Integer.parseInt(sc.nextLine());
+        str = Integer.toString(in);
+        for (int i = 0; i < str.length(); i++) {
+            out += Character.getNumericValue(str.charAt(i));
+        }
+        System.out.println("Total = " + out);
+        sc.close();
+    }
 }
