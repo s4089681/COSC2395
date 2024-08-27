8c2440f98a4cb29c8c9b2f4a34fa999a4f764532
Harrison Black <harrison@Harrisons-MacBook-Pro.local>
2024-08-27 19:43:14 +1000

    Main class testing

--- assignment_1/MtBullerResort/MtBullerResort.java
+++ assignment_1/MtBullerResort/MtBullerResort.java
@@ -1,6 +1,26 @@
 package MtBullerResort;
 
-public class MtBullerResort {
+import MtBullerResort.Store.Accomodation.Accomodation;
+import MtBullerResort.Store.Accomodation.AccomodationType;
+import MtBullerResort.Store.TravelPackage.TravelPackage;
+import MtBullerResort.Customer.*;
 
+import java.util.ArrayList;
+
+public class MtBullerResort {
+	private ArrayList<TravelPackage> packages;
+	private ArrayList<Customer> customers;
+	private ArrayList<Accomodation> accomodation; // Contains all avilable accomodations
+
+	// public fn loadAccomodation
+	// public fn loadPackages
+	// public fn loadCustomers
+	public static void main(String[] args) {
+
+		ArrayList<Accomodation> a = new ArrayList<>();
+		a.add(new Accomodation(1.0, AccomodationType.APARTMENT));
+		a.add(new Accomodation(2.0, AccomodationType.HOTEL));
+		Accomodation.writeAccomodation(a);
+	}
 
 }
